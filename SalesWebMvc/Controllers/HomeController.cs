03b233f2fa05b5fbf0db9a7ac72706d25cca43fb
Guilherme-Daneliv23 <gui.professional.profile@gmail.com>
2024-10-21 14:01:41 -0300

    Criando o primeiro controller e fazendo testes nas páginas Razor

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC pp from C# Course";
+            ViewData["Student"] = "Guilherme daneliv";
+            ViewData["Professor"] = "Nelio Alves";
 
             return View();
         }
